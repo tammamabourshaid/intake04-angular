f3b6ee273be29a735039d579dba7a5f0601789f6
tabourshaid <tabourshaid@users.noreply.github.com>
2019-05-12 12:15:59 +0200

    update Task Manager Componenet

--- src/app/task-manger/task-manger.component.ts
+++ src/app/task-manger/task-manger.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-task-manger',
   templateUrl: './task-manger.component.html',
-  styleUrls: ['./task-manger.component.scss']
+  styleUrls: ['./task-manger.component.scss'],
 })
 export class TaskMangerComponent implements OnInit {
 
